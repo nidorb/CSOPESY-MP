6900a9fef151c0b3c75074437f86c0d9a68d3a01
ricardolu1s <113566864+ricardolu1s@users.noreply.github.com>
2024-10-11 18:51:22 +0800

    initial print -ls

--- Process.h
+++ Process.h
@@ -17,15 +17,22 @@ public:
 
     int commandCtr;
     int cpuCoreID = -1;  // Core ID that executed OR is executing the process
-
+    int progress;
+    int totalWork;
     ProcessState currentState = READY;
 
     static int next_pid;
 
     // Constructor
-    Process(string name, string timestamp) : name(name), timestamp(timestamp) {
+    Process(string name, string timestamp, int cpuCoreID, int progress, int totalWork, ProcessState state) : 
+        name(name), timestamp(timestamp), cpuCoreID(cpuCoreID), progress(progress), totalWork(totalWork), currentState(state) {
         if (!name.empty()) {
             pid = next_pid++;
+        }
+        if (currentState == RUNNING) {
+            if (progress == totalWork) {
+                currentState = FINISHED;
+            }
         }
     }
 
@@ -35,6 +42,10 @@ public:
 
     string getProcessName(){
         return name;
+    }
+
+    string getProgressString(){
+        return std::to_string(progress) + " / " + std::to_string(totalWork);
     }
 };
 
